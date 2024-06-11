496cacdfad49734717fc843445720898d10179bc
Elliot Saba <staticfloat@gmail.com>
2024-06-11 19:05:48 +0000

    GCCBootstrap v9.0.0

--- src/GCCBootstrap_jll.jl
+++ src/GCCBootstrap_jll.jl
@@ -1,9 +1,5 @@
-# Use baremodule to shave off a few KB from the serialized `.ji` file
-baremodule GCCBootstrap_jll
-using Base
-using Base: UUID
-import JLLWrappers
+module GCCBootstrap_jll
+using LazyJLLWrappers
+@generate_jll_from_toml()
+end # module GCCBootstrap_jll
 
-JLLWrappers.@generate_main_file_header("GCCBootstrap")
-JLLWrappers.@generate_main_file("GCCBootstrap", UUID("7627cfbf-f290-59f7-b5e8-595c7b62b918"))
-end  # module GCCBootstrap_jll
